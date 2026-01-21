02898d4ff4aa6366e2ca5ad9de5f67488128b19b
Sven Kleinert <sven.kleinert@phoenixd.uni-hannover.de>
2026-01-21 14:05:16 +0100

    typo fixed.

--- zope_files/healthcheck.py
+++ zope_files/healthcheck.py
@@ -4,7 +4,7 @@ from urllib.error import HTTPError
 from urllib.request import Request, urlopen
 
 try:
-    with urlopen(Request("http://localhest:8080"), timeout=4) as response:
+    with urlopen(Request("http://localhost:8080"), timeout=4) as response:
         sys.exit(0 if response.status < 500 else 1)
 except HTTPError as e:
     sys.exit(0 if e.code < 500 else 1)
